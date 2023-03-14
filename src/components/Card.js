e9285c2116b9d0b6545d39986880ccf6f71003ed
josh-zuver <joshua.d.zuver@gmail.com>
2023-03-14 11:19:22 -0400

    added comments to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,13 @@
-
+/* Wrapper class to reduce code duplication
+   Used in Expenses and ExpenseItem components
+*/
 import "./Card.css";
 
 function Card(props){
+    //to preserve styling
     const classes = "card " + props.className;
-    return  <div className={classes}>{props.children}</div>;
 
+    return  <div className={classes}>{props.children}</div>;
 }
 
 export default Card;
